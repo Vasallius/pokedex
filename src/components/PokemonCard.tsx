365dd69a79d991175822e8cf60dec1f31ff3cfe7
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-08-19 00:05:16 +0800

    feat: set basis to make flex box no of items responsive

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,6 @@
 export function PokemonCard() {
   return (
-    <div className="card card-normal w-96 bg-base-100 shadow-xl">
+    <div className="card card-normal w-96 bg-base-100 shadow-xl basis-1/4 lg:basis-1/5">
       <figure>
         <img
           src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
