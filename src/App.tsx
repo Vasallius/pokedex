a5e3a675adec782b1ace1758246b4f610529af88
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-08-19 00:13:36 +0800

    feat: display pokemon names

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { PokemonCard } from "./components/PokemonCard";
+
+type ApiData = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Pokemon[];
+};
+
+type Pokemon = {
+  name: string;
+  url: string;
+};
 
 function App() {
-  return <>Pokedex</>;
+  const [data, setData] = useState<ApiData | null>(null);
+  useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=14")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  if (data) {
+    console.log(data.results);
+  }
+  return (
+    <>
+      <h1>Pokedex</h1>
+      <div className="flex flex-row flex-wrap">
+        {data &&
+          data.results.map((pokemon) => {
+            return <PokemonCard name={pokemon.name} link={pokemon.url} />;
+          })}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,13 @@
-export function PokemonCard() {
+type PokemonCardProps = {
+  name: string;
+  link: string;
+};
+
+function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function PokemonCard({ name, link }: PokemonCardProps) {
   return (
     <div className="card card-normal w-96 bg-base-100 shadow-xl basis-1/4 lg:basis-1/5">
       <figure>
@@ -9,10 +18,11 @@ export function PokemonCard() {
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Bulbasaur #001</h2>
+        <h2 className="card-title">{capitalize(name)} #001</h2>
         <div className="badge badge-secondary">Grass</div>
         <p></p>
       </div>
+      <h2>{link}</h2>
     </div>
   );
 }
