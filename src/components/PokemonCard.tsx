00233b212554badb8e9c20b5985a052dfcb523ab
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-08-18 23:10:44 +0800

    feat: componentize pokemon card

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,18 @@
+export function PokemonCard() {
+  return (
+    <div className="card card-normal w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+          className="w-48"
+          alt="Picture of Pokemon"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Bulbasaur #001</h2>
+        <div className="badge badge-secondary">Grass</div>
+        <p></p>
+      </div>
+    </div>
+  );
+}
