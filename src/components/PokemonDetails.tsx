68fa5f1882c5c3a227e3dacd61d606c056e6ea40
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-08-20 16:48:46 +0800

    feat: change button colors

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -44,38 +44,47 @@ export function PokemonDetails() {
   if (pokemonData) {
     return (
       <>
-        <div className="flex justify-center mt-12">
-          <div className="card card-side max-w-3xl bg-base-100 shadow-xl p-8">
-            <figure>
-              <img
-                src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${
-                  pokemonData ? padNumber(id) : "001"
-                }.png`}
-                alt="Picture of Pokemon"
-                className="w-2/3"
-              />
-            </figure>
-            <div className="card-body">
-              <h1 className="card-title">
-                # {padNumber(id)} {capitalize(name)}
-              </h1>
-              <div>{pokemonTypes}</div>
-              <div>Height: {height}</div>
-              <div>Weight: {weight}</div>
-              {stats.map((stat: Stat) => (
-                <div key={stat.stat.name}>
-                  {capitalize(stat.stat.name)}: {stat.base_stat}
-                </div>
-              ))}
-
+        <div className="flex flex-col items-center justify-center mt-12">
+          <div className="w-full flex flex-col items-center">
+            <div className="card card-side max-w-3xl bg-base-100 shadow-xl p-8 w-full mb-12">
+              <figure>
+                <img
+                  src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${
+                    pokemonData ? padNumber(id) : "001"
+                  }.png`}
+                  alt="Picture of Pokemon"
+                  className="w-2/3"
+                />
+              </figure>
+              <div className="card-body">
+                <h1 className="card-title">
+                  # {padNumber(id)} {capitalize(name)}
+                </h1>
+                <div>{pokemonTypes}</div>
+                <div>Height: {height}</div>
+                <div>Weight: {weight}</div>
+                {stats.map((stat: Stat) => (
+                  <div key={stat.stat.name}>
+                    {capitalize(stat.stat.name)}: {stat.base_stat}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="flex items-center gap-8">
               <Link to="/">
-                <button className="btn btn-primary">Home</button>
+                <button className="btn btn-primary normal-case bg-[#3e7dca]">
+                  Home
+                </button>
               </Link>
               <Link to={`/${id === 1 ? id : id - 1}`}>
-                <button className="btn btn-primary">Prev</button>
+                <button className="btn btn-primary normal-case bg-[#3e7dca]">
+                  Previous
+                </button>
               </Link>
               <Link to={`/${id === 1010 ? id : id + 1}`}>
-                <button className="btn btn-primary">Next</button>
+                <button className="btn btn-primary normal-case bg-[#3e7dca]">
+                  Next
+                </button>
               </Link>
             </div>
           </div>
