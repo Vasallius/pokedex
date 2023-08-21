7cda3c7c0805c0cc3b658fdd3edd8b9dd7ae1504
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-08-21 23:33:20 +0800

    feat: add home icon

--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -1,11 +1,11 @@
+import { BiHomeAlt2 } from "react-icons/bi";
 import { Link } from "react-router-dom";
-
 export function NavigationButtons({ id }: { id: number }) {
   return (
     <div className="flex items-center gap-8">
       <Link to="/">
         <button className="btn text-white normal-case font-primary bg-[#3e7dca]">
-          Home
+          <BiHomeAlt2 /> Home
         </button>
       </Link>
       <Link to={`/${id === 1 ? id : id - 1}`}>
