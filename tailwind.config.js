cc51694f39a53c656917c3bd84b631c1aeb483be
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-08-21 22:48:20 +0800

    feat: change font to DM Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["DM Sans"],
+      },
+    },
   },
   daisyui: {
     themes: ["dark"],
